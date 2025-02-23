a5c059a66ef1db56ab3406690baf701b16200bf8
MoonHoplite <ant_nebula@163.com>
2025-02-23 20:50:03 +0800

    Modified PPO and Buffer. Add a demo video.

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import model
 import trainer
 
 
-
 # DDQN training
-chk_name = r"saved_models\flappybird_ddqn.pth"
-args = trainer.TrainingArguments(load_checkpoint = chk_name, observe=0, explore=1, batch_size=256)
-trainer = trainer.DQN(args)
-trainer.train()
+# chk_name = r"saved_models\flappybird_ddqn.pth"
+# args = trainer.TrainingArguments(load_checkpoint = chk_name, initial_epsilon=0.0001, observe=10000, explore=1, batch_size=128, lr_value=1e-6, replay_memory=10000)
+# trainer = trainer.DQN(args)
+# trainer.train()
 
 # VPG training
-# chk_name = (r"saved_models\flappybird_vpg_actor_100.pth", r"saved_models\flappybird_vpg_critic_100.pth")
-# args = trainer.TrainingArguments(batch_size=1024, save_interval=100, load_checkpoint = chk_name)
+# chk_name = (r"saved_models\flappybird_vpg_actor_0.pth", r"saved_models\flappybird_vpg_critic_0.pth")
+# args = trainer.TrainingArguments(horizon = 8192, save_interval=50, load_checkpoint = chk_name)
 # trainer = trainer.VPG(args)
 # trainer.train()
 
 
 # PPO training
-# chk_name = (r"saved_models\flappybird_ppo_actor_800.pth", r"saved_models\flappybird_ppo_critic_800.pth")
-# args = trainer.TrainingArguments(batch_size=1024, save_interval=100, load_checkpoint = chk_name)
+# chk_name = (r"saved_models\flappybird_ppo_actor_0.pth", r"saved_models\flappybird_ppo_critic_0.pth")
+# args = trainer.TrainingArguments(horizon = 8192, save_interval=50, lr_policy=1e-5, lr_value=1e-4, load_checkpoint = chk_name)
 # trainer = trainer.PPO(args)
 # trainer.train()
